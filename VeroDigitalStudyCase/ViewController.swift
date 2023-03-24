98f62daed55c8ef325385909fa60c6c583a05ef5
MehmetBilirr <mehmetbilir@aol.com>
2023-03-24 20:07:04 +0300

    Initial Commit

---
+++ VeroDigitalStudyCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeroDigitalStudyCase
+//
+//  Created by Mehmet Bilir on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
